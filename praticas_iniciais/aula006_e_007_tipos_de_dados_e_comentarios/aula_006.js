ac9034bd8e07ef2fe80f77af11fdda81f82d81bc
Leonardo-Broinizi <leonardobroinizidefarias@gmail.com>
2024-10-06 19:02:46 -0300

    Práticas iniciais.

---
+++ praticas_iniciais/aula006_e_007_tipos_de_dados_e_comentarios/aula_006.js
@@ -0,0 +1,19 @@
+
+let nome = 'Leonardo' // string
+let idade = 90  // number
+let logado = false // boolean
+let lista = ['ovo', 'massa', 'corante', 30] // array
+let nomeCompleto = { nome: 'Bonieky', idade: 90 } // object
+
+console.log(lista[2])
+console.log(nomeCompleto[nome])
+
+// o comando abaico exibe os dados no CONSOLE
+console.log(typeof(nomeCompleto))
+
+/*
+Este é um comentário 
+de várias linhas
+só porque eu quero.
+ */
+
---
+++ praticas_iniciais/aula008_exercicios_de_variaveis/exercicios.js
@@ -0,0 +1,17 @@
+// ex 01: Crie uma variável para armazenar o nome do carro e ponha o valor Ferrari. E exiba-a. 
+
+let carro = 'Ferrari'
+console.log(carro)
+
+// ex 02: Cria uma variável para armazenar o preço do bolo, que custa R$ 19,99. E exiba-a.
+
+let preçoDoBolo = 19.99
+console.log(`O preço do bolo é ${preçoDoBolo}`)
+
+
+// ex 03: Corrija o código a seguir (e exiba-o). 
+// let "cidade" São Paulo
+
+let cidade = 'São Paulo'
+console.log(cidade)
+

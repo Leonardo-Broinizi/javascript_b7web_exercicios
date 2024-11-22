6282cf2d82717ee6f9f2a4616550afb1a669bc83
Leonardo-Broinizi <leonardobroinizidefarias@gmail.com>
2024-11-22 13:54:40 -0300

    .

--- modulo_03/aula_12_eventos_de_teclado_pt-2/script.js
+++ modulo_03/aula_12_eventos_de_teclado_pt-2/script.js
@@ -1,11 +1,7 @@
 // Anotações: Com os comandos do parâmetro da 
 
 function soltou(event) {
-    console.log(event.key);
-    console.log('Tecla apertada: ' + event.code);
-    console.log('Com o Shift? ' + event.shiftKey);
-    console.log('Com o CTRL? ' + event.ctrlKey);
-    console.log('Com o ALT? ' + event.altKey);
+    console.log('SOLTOU!');
 }  
  
 const input = document.querySelector('input');
